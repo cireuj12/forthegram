35317f11d6ed398c87d302e7b8820d28491483a4
App Academy Student <aa@Apps-Mac-mini-2.local>
2019-05-28 15:08:36 -0400

    before branch

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './frontend/index.jsx',
+    entry: './frontend/forthegram.jsx',
     output: {
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
         filename: 'bundle.js'
