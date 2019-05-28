4a5538b54270f61a4ec507b8e8ff9db24a9e0a7b
App Academy Student <aa@Apps-Mac-mini-2.local>
2019-05-28 15:46:45 -0400

    before sessions

--- db/migrate/20190528190034_create_users.rb
+++ db/migrate/20190528190034_create_users.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
+
     create_table :users do |t|
       t.string :username, null: false
       t.string :email, null: false
@@ -8,9 +9,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
-    add_index :users, :email,
+    add_index :users, :email
     add_index :users, :username, unique: true
     add_index :users, :session_token, unique: true
-    end
+    
   end
 end
