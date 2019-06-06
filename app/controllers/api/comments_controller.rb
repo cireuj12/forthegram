639668709d50b858281bc0841b7fee3950306a74
cireuj12 <ericju12@gmail.com>
2019-06-06 10:40:25 -0400

    comments controller fix

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,6 +1,6 @@
 class Api:CommentsController < ApplicatonController
 
-    before_action: :require_logged_in
+    before_action :require_logged_in
 
     def create 
         @comment = current_user.comments.new(comment_params)
